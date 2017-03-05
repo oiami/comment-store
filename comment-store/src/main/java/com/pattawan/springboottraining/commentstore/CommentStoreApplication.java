bd5ec1d9efdb7e51fb87d79adc515f560aa19afd
Pattawan Kaewduangdee <pattawanna@gmail.com>
2017-03-05 19:44:19 +0100

    Import resource inside class path

--- comment-store/src/main/java/com/pattawan/springboottraining/commentstore/CommentStoreApplication.java
+++ comment-store/src/main/java/com/pattawan/springboottraining/commentstore/CommentStoreApplication.java
@@ -3,9 +3,11 @@ package com.pattawan.springboottraining.commentstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EntityScan(basePackages= {"com.pattawan.springboottraining"})
+@ImportResource(value={"classpath*:legacy-context.xml"})
 public class CommentStoreApplication {
 
 	public static void main(String[] args) {
